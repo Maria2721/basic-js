f8c9fb19ad66e7829b3b7a29c266af38861e40ef
Maria2721 <mashaivvas@gmail.com>
2025-01-07 05:35:48 +0300

    feat: add vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,32 +1,111 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(modification = true) {
+    this.modification = modification;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw Error('Incorrect arguments!');
+    }
+
+    let str = [];
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const alphabetArr = alphabet.split('');
+
+    // Create keyword of length equal to message
+    let messageArr = message.toUpperCase().split('');
+    let keyArr = key.toUpperCase().split('');
+    let keyMessageArr = [];
+    let pass = 0;
+
+    for (let i = 0; i < messageArr.length; i++) {
+      if (alphabetArr.includes(messageArr[i])) {
+        let character = keyArr[(i - pass) % keyArr.length];
+        keyMessageArr.push(character);
+      } else {
+        keyMessageArr.push(messageArr[i]);
+        pass += 1;
+      }
+    }
+
+    // Encrypt message by keyword
+    for (let i = 0; i < messageArr.length; i++) {
+      let original = alphabetArr.indexOf(messageArr[i]);
+      let shift = alphabetArr.indexOf(keyMessageArr[i]);
+
+      if (original === -1) {
+        str.push(messageArr[i]);
+      } else {
+        let encryptedIndex = (original + shift) % alphabetArr.length;
+        let encrypted = alphabetArr[encryptedIndex];
+        str.push(encrypted);
+      }
+    }
+
+    return this.modification ? str.join('') : str.reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) {
+      throw Error('Incorrect arguments!');
+    }
+
+    let str = [];
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const alphabetArr = alphabet.split('');
+
+    // Create keyword of length equal to message
+    let messageArr = encryptedMessage.toUpperCase().split('');
+    let keyArr = key.toUpperCase().split('');
+    let keyMessageArr = [];
+    let pass = 0;
+
+    for (let i = 0; i < messageArr.length; i++) {
+      if (alphabetArr.includes(messageArr[i])) {
+        let character = keyArr[(i - pass) % keyArr.length];
+        keyMessageArr.push(character);
+      } else {
+        keyMessageArr.push(messageArr[i]);
+        pass += 1;
+      }
+    }
+
+    // Decrypt message by keyword
+    for (let i = 0; i < messageArr.length; i++) {
+      let encrypted = alphabetArr.indexOf(messageArr[i]);
+      let shift = alphabetArr.indexOf(keyMessageArr[i]);
+
+      if (encrypted === -1) {
+        str.push(messageArr[i]);
+      } else {
+        let originalIndex = (encrypted - shift + alphabetArr.length) % alphabetArr.length;
+        let original = alphabetArr[originalIndex];
+        str.push(original);
+      }
+    }
+
+    return this.modification ? str.join('') : str.reverse().join('');
   }
 }
 
